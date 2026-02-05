59af0c165acde25000456fb5efb054a5bc1aff86
KrzysztofDrezek <0Drezk79@solent.ac.uk>
2026-02-05 20:17:07 +0000

    Chore: enable JUnit5 desktop tests

---
+++ test/com/group/ticketmachine/SanityTest.kt
@@ -0,0 +1,11 @@
+package com.group.ticketmachine
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class SanityTest {
+    @Test
+    fun sanity() {
+        assertTrue(true)
+    }
+}
